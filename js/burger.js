819d5da3494af13ad26706df2fe8b3e0d3c5c5aa
joseDuc <ppgq@live.com>
2024-06-30 18:32:00 +0200

    Sección Testimonial creada

---
+++ js/burger.js
@@ -0,0 +1,10 @@
+document.querySelector(".nav-burger").addEventListener("click", function () {
+  document.querySelector(".header-nav-links-vertical").classList.toggle("nav-active");
+  document.querySelector(".nav-burger").classList.toggle("grey-background");
+  const burgerLines = document.querySelectorAll(".nav-burger .line");
+  burgerLines.forEach((line) => {
+    line.classList.toggle("white");
+  });
+
+  document.body.classList.toggle("no-scroll");
+});
